94f3b4ee4eee19da439a4cf2ac936a3ee819a24e
Liam Read <Liam@liam.pro>
2017-05-22 15:34:19 +0100

    Added Node Sass and Bootstrap 4 :octocat:

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>{{ message }}</h1>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
@@ -25,7 +26,8 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Cyanic',
+      message: 'hi paul'
     }
   }
 }
