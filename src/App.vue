9ab5bfb24f479f18c2c6d526bb7de3ebef01d8b9
Liam Read <Liam@liam.pro>
2017-05-24 05:56:20 +0100

    Revert "Global Variables & Mixins File and Vue Template update :space_invader:"
    This reverts commit 090e580cacbe440b002a2f80d0ac21ce4db65641.

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,13 @@
 
   // Global variables outline in app so that they can have scope over other objects that are scoped to components.
   // Colour Palette Reference https://puu.sh/vVbjW/f802be9239.png
-  @import "./assets/master";
+  $primaryColour: #027ef1;
+  $secondaryColour: #026ed2;
+  $primaryWhite: #fcfcfd;
+  $primaryDark: #162030;
+  $lightGrey: #d7d9db;
+  $darkGrey: #c9b3c1;
+  $fontFamily: 'Oxygen', sans-serif;
 
   #app {
     @import url('https://fonts.googleapis.com/css?family=Oxygen');
--- src/components/Homepage/CallToAction.vue
+++ src/components/Homepage/CallToAction.vue
@@ -23,9 +23,11 @@
   }
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
   // Start coding!
-  @import "../../assets/master";
+
+  $secondaryColour: #026ed2;
 
   .CallToAction {
 
