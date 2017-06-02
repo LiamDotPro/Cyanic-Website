9141995b715f3211435ec45ba46da64be4009684
Liam Read <Liam@liam.pro>
2017-06-02 19:35:00 +0100

    Signature footer update #36
    Signature footer updated

--- src/components/SignatureFooter.vue
+++ src/components/SignatureFooter.vue
@@ -3,11 +3,28 @@
     <div class="container">
       <div class="row">
         <div class="col-xs col-lg-4 team-text">
-          <span><icon name="code" scale="0.8"></icon> + <icon name="paint-brush" scale="0.7"></icon> = The Cyanic Team </span>
+          <span class=""> © 2017 Cyanic Solutions </span>
+        </div>
+        <div class="col-xs col-lg-4 social-icons-text">
+          <a href="" target="_blank">
+            <icon name="github"></icon>
+          </a>
+          <a href="" target="_blank">
+            <icon name="facebook"></icon>
+          </a>
+          <a href="" target="_blank">
+            <icon name="twitter"></icon>
+          </a>
+          <a href="" target="_blank">
+            <icon name="linkedin"></icon>
+          </a>
         </div>
         <div class="col-xs col-lg copyright-text float-right">
           <div class="float-right">
-            <span class=""> © 2017 Cyanic Solutions </span>
+            <icon name="paint-brush" scale="0.9"></icon>
+            +
+            <icon name="code"></icon>
+            <span>The Cyanic Team</span>
           </div>
         </div>
       </div>
@@ -31,8 +48,22 @@
   @import "/../assets/master";
 
   .signature-footer {
-text-align: left;
+    text-align: left;
     color: $darkGrey;
+    background: $primaryWhite;
+    border-top: #d7d9db 1px solid;
+
+    .social-icons-text {
+      text-align: center;
+
+      a {
+        margin-left: 2px;
+        margin-right: 2px;
+        text-decoration: none;
+        color: $darkGrey
+      }
+
+    }
 
     .row {
       padding: 10px 10px;
@@ -44,7 +75,7 @@ text-align: left;
     }
 
     .team-text {
-      font-size:14px;
+      font-size: 14px;
       padding-left: 0;
     }
   }
