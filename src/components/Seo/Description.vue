6ed1d1300e7e5ebe188f539a37cd3bc1f10d2a15
Liam Read <Liam@liam.pro>
2017-08-29 16:12:42 +0100

    Seo Content

--- src/components/Seo/Description.vue
+++ src/components/Seo/Description.vue
@@ -3,26 +3,55 @@
 
     <div class="container">
       <div class="row align-items-center">
+        <div class="col-lg-3">
+
+        </div>
         <div class="col">
           <h2>Where do we offer our SEO Services?</h2>
-          <p>Similar to a lot of other agencies and search engine optimisers we don’t just offer “SEO Derby” or “SEO Reading”. Although members of our team are based in Derby and Reading we aren’t  limited to helping websites specifically in those areas. As we are a small, currently two person, remote team we are already adapt and experienced with working with clients all over the UK. The effectiveness of the SEO work we complete isn’t affected by location as it is the same search engine algorithm determining the website’s ranking for the respective keywords; the only difference is in that there will be varying competition for keywords in different locations. </p>
-          <p>However, if locally there are a lot of businesses competing for similar or the same keywords you can still with our help rise above those who are less engaged or that have neglected the search engine optimisation of their website. Although we are not against it we don’t visit every client’s physical business locations, we instead work with clients remotely (via the most convenient form of communication for them) as we are a small team and want all clients to benefit from our cost efficiency.</p>
+          <p>
+            Similar to a lot of other agencies and search engine optimisers we don’t just offer “SEO Derby” or “SEO Reading”. Although members of our team are based in Derby and Reading we aren’t limited to helping websites specifically in those areas. As we are a small, currently two person, remote team we are already adept and experienced with working with clients all over the UK. The effectiveness of the SEO work we complete isn’t affected by location as it is the same search engine algorithm determining the website’s ranking for the respective keywords; the only difference is in that there will be varying competition for keywords in different locations.
+          </p>
+          <p>
+            However, if locally there are a lot of businesses competing for similar or the same keywords you can still help rise above those who are less engaged or that have neglected the search engine optimisation of their website. Although we are not against it we don’t visit every client’s physical business locations, we instead work with clients remotely (via the most convenient form of communication for them) as we are a small team and want all clients to benefit from our cost efficiency.
+          </p>
         </div>
       </div>
       <div class="row align-items-center">
         <div class="col">
           <h2>What is SEO and how do we approach it?</h2>
-          <p>Search Engine Optimisation, often referred to as SEO is the process of changing both “on page” and “off page” factors that influence how highly search engine’s (Google, Bing, etc.) value the content when users search for specific keywords.</p>
-          <p>If not familiar with SEO “on page” factors relate to changing / improving the structure and content of the website’s pages to make it more appropriate to answer the questions the search engine users are asking. On page SEO is the majority of the work that we will do for you when you chose us to optimise your website. We understand that a lot of agencies and other search engine optimisers will claim to offer a wider offering, however, we understand that you can achieve great ranking results by primarily focusing on the part of SEO that you have the most control over. With better content, structure and visibility to the search engines your website will look and be more appealing to both users and other websites that may want to link to your site as a reference.</p>
-          <p>Having your website linked to from external websites is an area of SEO commonly referred to as “off page SEO” that includes “backlinking”, “outreach” and “directory listings”. We don’t claim to be experienced at the actions of approaching or enticing highly (or better) ranking websites than our client’s for the purpose of creating a backlink and positively enhancing their website’s ranking. </p>
+          <p>
+            Search Engine Optimisation, often referred to as SEO is the process of changing both “on page” and “offpage” factors that influence how highly a search engine (Google, Bing, etc.) value the content when users search for specific keywords.
+          </p>
+          <p>
+            If not familiar with SEO “on page” factors relate to changing / improving the structure and content of the website’s pages to make it more appropriate to answer the questions the search engine users are asking. On page SEO is the majority of the work that we will do for you when you chose us to optimise your website. We understand that a lot of agencies and other search engine optimisers will claim to offer a wider offering, however, we understand that you can achieve great ranking results by primarily focusing on the part of SEO that you have the most control over. With better content, structure and visibility to the search engines your website will look and be more appealing to both users and other websites that may want to link to your site as a reference.
+          </p>
+          <p>
+            Having your website linked to from external websites is an area of SEO commonly referred to as “off page SEO” that includes “backlinking”, “outreach” and “directory listings”. We don’t claim to be experienced at the actions of approaching or enticing highly (or better) ranking websites than our client’s for the purpose of creating a backlink and positively enhancing their website’s ranking.
+          </p>
         </div>
       </div>
       <div class="row align-items-center">
         <div class="col">
           <h2>What does the SEO work involve?</h2>
-          <p>An overview of the work (and expectant order of actions) involved in the search engine optimisation process we follow includes:</p>
+          <p>
+            An overview of the work (and expectant order of actions) involved in the search engine optimisation process we follow includes:</p>
           <h3>Initial Setup</h3>
-          <p>Every website (that hasn’t had SEO work done before) starts off by enabling and installing tools to monitor/identify the current state and going forward the progress of the website’s keyword ranking. These tools allow a clearer view of how the changes made to the website have impacted the SEO as well as the subsequent benefits of doing so (e.g. increased visitors). With this we are also more than willing to help the website owners become familiar with using these tools to give them the ability to review the site themselves during and after we are working on it with them.  </p>
+          <p>
+            Every website (that hasn’t had SEO work done before) starts off by enabling and installing tools to monitor / identify the current state and going forward the progress of the website’s keyword ranking. These tools allow a clearer view of how the changes made to the website have impacted the SEO as well as the subsequent benefits of doing so (e.g. increased visitors). With this we are also more than willing to help the website owners become familiar with using these tools to give them the ability to review the site themselves during and after we are working on it with them. </p>
+          <h3>Understanding your business and identifying keywords</h3>
+          <p>
+            We often carry out research into a plethora of different keywords that might be used as part of your campaign, this includes finding keywords which might be deemed as ‘quick wins’ which bolster your impressions and engagement. This also allows us to lay the foundations towards ranking highly for more competitive areas as understanding how the viewership is spread out is a key part of understand how to get the most out of your written content. Strategizing around not only what you offer now but towards new and innovative areas that are rising can really pay dividends towards your overall objective as a company which is what makes our seo services more unique and better. Doing this allows us to stay sharp and ahead of the competition and provides out clients with different avenues to make the online presence even more successful.</p>
+          <h3>Optimization of page layout</h3>
+          <p>
+            An important part of seo is to get all of the page content correct and also apply appropriate structure to the related content which will make it more likely to register with google. This is done through various methods which often change in accordance with the google algorithm, though we find it to be a mixture of providing formatting appropriate for real people alongside optimization. This is because ultimately even if your website ranks the top for a keyword if the pages fails at converting users then it might as well have not been optimised in the first place.</p>
+          <h3>Analysis, suggestion and implementation of content</h3>
+          <p>
+            Content is king for all websites, having a better set of content even if it’s less optimised could be the bridge between converting a larger percentage of the potential customers/clients viewing your website. We’ve previously made small changes and added new features to websites that don’t overall change the website very much but the engagement with those new features has brought in a lot of new traffic providing leads for our clients. An example of this is presented on the homepage with the news tracker for method which brought in many new users alongside new agreements being for advertising and collaboration deals. We played a key role within the analysis and suggestion of changes as well as ultimately delivering the feature sets to the client which lead to this success.</p>
+          <p>
+            After carrying out an analysis our findings will be presented to you in the most appropriate format depending on how much control you wish to have over the changes, some clients prefer we just get on with providing the service and are happy with the results. Others prefer to be more engrained in the process and provide their own flavour and style which will ultimately affect the outcome of the end product. We have found that both of these approaches have ups and downs but the outcome has always been in the benefit of the client utilising the SEO service we provide.</p>
+          <h3>Reviewing & reporting impact of changes</h3>
+          <p>
+            We use some of the most modern tools and techniques to help analyse and improve various aspect of your website. For the client this generically means providing reporting which shows how our service has impacted their rankings and what direction the project is moving in, all the reporting is as simple or complex as is required to make it consumable for the client. We have found that tailoring the reporting has meant that clients don’t feel like they're paying for something that they find hard to understand or don’t see the benefit of.</p>
         </div>
       </div>
     </div>
--- src/components/Seo/Faq.vue
+++ src/components/Seo/Faq.vue
@@ -4,21 +4,23 @@
       <div class="row align-items-center">
         <div class="col-12">
           <div class="testimonial-tile">
-            <h2>Can esports/gaming websites benefit from SEO?</h2>
+            <h2>Can esports / gaming websites benefit from SEO?</h2>
             <p>For anyone reading this who are unfamiliar with “esports” it is a term referring to the competitive side of video games. With tournaments awarding prize pools up to as much of $24.7 million every year, often with a wide range of smaller yet still noticeably grand prizes for the winners happening most months it is a flourishing and growing market.</p>
-            <p>Like most owners of business and personal websites who want their website to be visited and engaged with by more users…</p>
+            <p>Like most owners of business and personal websites, who want their website to be visited and engaged with by more users, esports teams, organisations and content websites want the same. With their goal to reach a wider audience than they already are ranking highly in search engines can really help them to be found by and capture the attention of potential fans and returning users. Although we understand that often SEO will rightfully not be as important as achieving higher results in the tournaments you compete in, it can really help focus attention of producing a better website and content that you can benefit from.</p>
           </div>
         </div>
         <div class="col-12">
           <div class="testimonial-tile">
             <h2>What results can we promise</h2>
-            <p>Honestly, with SEO we feel it takes a great deal of experience to promise great SEO results for every business that you work with to get the results they really want.</p>
+            <p>
+              Honestly, with SEO we feel it takes a great deal of experience to promise guaranteed SEO results for every business that you work with to get the results they really want. What we can promise is that while optimising your website it will gradually improve on it’s own merits which means it will be ready to tackle any search engine changes. Many other companies often utilise black hat techniques which in the short term provide results but aren't good in the long term for any SEO campaign, we promise to create stability and results that will work over the long term and won’t have your website flagged or filtered from results.</p>
           </div>
         </div>
         <div class="col-12">
           <div class="testimonial-tile">
             <h2>Reporting on the results</h2>
-            <p>We understand that if you haven’t experienced SEO and the process before it may be worrying or confusing that after the first changes have been made to the website and you search on Google for your business that it isn’t the first or even on the first page of results. Although its common knowledge amongst… </p>
+            <p>
+              We understand that if you haven’t experienced SEO and the process before it may be worrying or confusing that after the first changes have been made to the website and you search on Google for your business that it isn’t the first or even on the second page of results. Patience is key as the search engine slowly moves your website up the rankings, from experience a website that slowly gets to the top of the results often has a far better chance of staying there for a longer period. Many online SEO guru’s will offer a service that gets you to the top quickly only for the search engine to blacklist the website or reposition it back to where it started. You can expect within the reporting to see a slow and steady approach where ground is being covered towards the best outcome for each client.</p>
           </div>
         </div>
       </div>
